854b2e28df59ddb52253334e64f7e8ddcdcc8385
ZhangH-W <102425240+ZhangH-W@users.noreply.github.com>
2025-01-31 12:15:43 +0000

    Deploying to gh-pages from @ ZhangH-W/zhangh-w.github.io@8ee6c5a12e1c30fd5ac792455664a04c65310e16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
+  },{id: "nav-journey",
+          title: "Journey",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Journey/";
+          },
+        },{id: "nav-research",
           title: "Research",
           description: "",
           section: "Navigation",
